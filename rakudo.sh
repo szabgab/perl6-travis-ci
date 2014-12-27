73d3986f919e312ac54c2b213d21929f8d8e2ab8
Gabor Szabo <gabor@szabgab.com>
2014-12-27 21:03:29 +0200

    experimental

---
+++ rakudo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+RAKUDO_BUILD_VERSION=2014.12.1
+pwd
+echo $RAKUDO_BUILD_VERSION
+# wget http://dwimperl.com/download/rakudo-$RAKUDO_BUILD_VERSION.tar.gz
+# tar xzf rakudo-$RAKUDO_BUILD_VERSION.tar.gz
+# prove -e "rakudo-$RAKUDO_BUILD_VERSION/bin/perl6 -Ilib 
