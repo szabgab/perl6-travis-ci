9745bfd2537cc4c518acbb502c71b0e071aa336e
Gabor Szabo <gabor@szabgab.com>
2014-12-31 20:31:54 +0200

    add destdir, otherwise panda did not work

--- install_module.sh
+++ install_module.sh
@@ -2,6 +2,9 @@
 
 RAKUDO_BUILD_VERSION=2014-12-1
 
+RAKUDO_PATH=/home/travis/rakudo-$RAKUDO_BUILD_VERSION
+
 echo Instaling $1
 
-/home/travis/rakudo-$RAKUDO_BUILD_VERSION/bin/perl6 /home/travis/rakudo-$RAKUDO_BUILD_VERSION/bin/panda install $1
+DESTDIR=$RAKUDO_PATH/languages/perl6 $RAKUDO_PATH/bin/perl6 $RAKUDO_PATH/bin/panda install $1
+
