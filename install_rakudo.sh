07534c8bc431565fe93722f683f035c6fdc5b2c1
Gabor Szabo <gabor@szabgab.com>
2014-12-31 22:22:18 +0200

    more experiments

--- install_rakudo.sh
+++ install_rakudo.sh
@@ -6,13 +6,16 @@ prove --version
 
 BASEDIR=$(dirname $0)
 $BASEDIR/version.sh
+cat $BASEDIR/version.sh
+source $BASEDIR/version.sh
 echo $RAKUDO_VERSION
+echo $RAKUDO_PATH
 
-#cd /home/travis/
-#wget http://dwimperl.com/download/rakudo-$RAKUDO_BUILD_VERSION.tar.gz
-#tar xzf rakudo-$RAKUDO_BUILD_VERSION.tar.gz
-#cd $OLD_DIR
-#
-## Run it once in order to avoid the error when the user really wants to install a module
-#DESTDIR=$RAKUDO_PATH/languages/perl6 $RAKUDO_PATH/bin/perl6 $RAKUDO_PATH/bin/panda
+cd /home/travis/
+wget http://dwimperl.com/download/rakudo-$RAKUDO_BUILD_VERSION.tar.gz
+tar xzf rakudo-$RAKUDO_BUILD_VERSION.tar.gz
+cd $OLD_DIR
+
+# Run it once in order to avoid the error when the user really wants to install a module
+DESTDIR=$RAKUDO_PATH/languages/perl6 $RAKUDO_PATH/bin/perl6 $RAKUDO_PATH/bin/panda
 
